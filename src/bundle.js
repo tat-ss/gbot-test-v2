b3952574a8c942588b3813cf6536e3d52b563c9b
Tat Jhetson <tat.jhetson@skyscanner.jp>
2017-06-30 17:26:24 +0900

    Add bundle example

---
+++ src/bundle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var root = document.getElementById('app');
+
+ReactDOM.render(
+  React.createElement('div', null, 'Hello from React'),
+  root
+);
