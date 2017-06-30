6dedd0d5c90a2acc77f6d50ad106e7487a401b07
Tat Jhetson <tat.jhetson@skyscanner.jp>
2017-06-30 13:03:50 +0900

    Initial commit

---
+++ docs/bundle.js
@@ -0,0 +1,18 @@
+var root = document.getElementById('app');
+
+loadjs([
+  'https://cdnjs.cloudflare.com/ajax/libs/react/15.6.1/react.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/react/15.6.1/react-dom.min.js'
+], 'react');
+
+loadjs.ready('react', {
+  success: function() {
+    ReactDOM.render(
+      React.createElement('div', null, 'Hello from React'),
+      root,
+    );
+  },
+  error: function() {
+    root.innerHTML = 'Deps not found';
+  },
+});
